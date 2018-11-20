f5c82ec33bde504432c8e21449cf0e6e91184965
itsmegh <42684917+itsmegh@users.noreply.github.com>
2018-11-20 13:47:50 -0700

    adding form submit to email functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,24 +14,7 @@
         }
       }
     });
-  
-    // Closes responsive menu when a scroll trigger link is clicked
-    $('.js-scroll-trigger').click(function() {
-      $('.navbar-collapse').collapse('hide');
-    });
-  
-    // Collapse Navbar
-    var navbarCollapse = function() {
-      if ($("#mainNav").offset().top > 100) {
-        $("#mainNav").addClass("navbar-shrink");
-      } else {
-        $("#mainNav").removeClass("navbar-shrink");
-      }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
+
   
     // Hide navbar when modals trigger
     $('.portfolio-modal').on('show.bs.modal', function(e) {
